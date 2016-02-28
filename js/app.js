3e076d86d4c57233a0f86b0fee3cf69677d1c286
rgowan <rane_00@hotmail.com>
2016-02-28 14:53:00 +0000

    adding some more functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(initialize);
 
 function initialize() {
+  $('a').on('click', function(e) {
+      e.stopPropagation();
+  });
+
   $('#card').on('click', function() {
     $(this).toggleClass('flipped');
   });
