6d442796e09ecadce27f61dac8ee1833721da57f
rgowan <rane_00@hotmail.com>
2016-03-10 14:25:12 +0000

    removing flip icon

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(initialize);
 
 function initialize() {
-  $('a').on('click', function(e) {
-      e.stopPropagation();
-  });
-
   $('#card').on('click', function() {
     $(this).toggleClass('flipped');
   });
 
-  $('.love ').on('click', function(e) {
+  $('a').on('click', function(e) {
+      e.stopPropagation();
+  });
+
+  $('.love').on('click', function(e) {
     e.stopPropagation();
     $(this).toggleClass('fa-heart-o fa-heart');
   });
