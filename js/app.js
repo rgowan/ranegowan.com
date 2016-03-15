e5669e73276ecac367a8df9c54f1e8985bd3cd13
rgowan <rane_00@hotmail.com>
2016-03-15 11:28:40 +0000

    restructuring js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 $(initialize);
 
 function initialize() {
+  clickEvents();
+  likeProject();
+}
+
+function clickEvents() {
   $('#card').on('click', function() {
     $(this).toggleClass('flipped');
   });
-
+  
   $('a, label, .like').on('click', function(e) {
       e.stopPropagation();
   });
-
-  likeProject();
 }
 
 function likeProject() {
   $('.project').each(function(){
     var counter = $(this).find('span').html();
-
     $(this).find('.like').one('click', function() {
-
       $(this).toggleClass('fa-heart-o fa-heart');
-
       if ($(this).hasClass('fa-heart')) {
         counter ++;
         $(this).parent().find('span').text(counter);
