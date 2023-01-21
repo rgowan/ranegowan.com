67fb773c4b323d1e8a0c0881c02c0416df5c2e3a
Rane Gowan <ranegowan@gmail.com>
2023-01-21 19:36:25 +0000

    update site

--- js/app.js
+++ js/app.js
@@ -1,78 +1,13 @@
 $(loaded);
 
-let windowBottom;
-let userScroll = 0;
-
 function loaded() {
   setTimeout(() => demo.init(), 1000);
 
-  window.addEventListener('resize', demo.resize);
-  window.addEventListener('scroll', getWindowHeight);
-  window.addEventListener('resize', getWindowHeight);
+  window.addEventListener("resize", demo.resize);
 
-  $('.burger').on('click', handleBurgerClick);
-  $('#nav li, .fa-sort-down').on('click', scrollToSection);
-
-  $('.quote-carousel').slick({
+  $(".recomendations-wrapper").slick({
     dots: true,
+    centerMode: true,
     arrows: false,
-    infinite: true,
-    autoplay: false,
-    autoplaySpeed: 10000,
-    speed: 500,
-    fade: false,
-    cssEase: 'linear'
   });
-}
-
-function scrollToSection() {
-  const element = $(this).attr('id');
-
-  $('.burger').removeClass('open');
-  $('.side-menu').removeClass('show-side-menu');
-  $('.shader').removeClass('dim');
-  $('body').removeClass('disable-scroll');
-
-  $('html, body').animate({
-    scrollTop: ($(`.${element}`).offset().top)
-  }, 2000);
-}
-
-function handleBurgerClick() {
-  $(this).toggleClass('open');
-  $('.side-menu').toggleClass('show-side-menu');
-  $('.shader').toggleClass('dim');
-  $('body').toggleClass('disable-scroll');
-}
-
-function getWindowHeight() {
-  userScroll = $(window).scrollTop();
-  windowBottom = userScroll + $(window).height();
-
-  animateNavbar();
-  animateSection('portfolio');
-  animateSection('experience');
-}
-
-function animateNavbar() {
-  const landingHeight = $('.landing').height();
-
-  userScroll > landingHeight ? $('nav').addClass('nav-small') : $('nav').removeClass('nav-small');
-}
-
-function animateSection(element) {
-  const elementTop = $(`.${element}`).offset().top;
-  const elemementBottom = elementTop + $(`.${element}`).height();
-
-  if(elemementBottom <= windowBottom) {
-    $(`.${element} .left, .${element} h4`)
-      .removeClass('hidden')
-      .addClass('animated fadeInLeft');
-
-    $(`.${element} .right`)
-      .removeClass('hidden')
-      .addClass('animated fadeInRight');
-
-    $(`.${element} hr`).removeClass('hidden');
-  }
 }
--- js/rain.js
+++ js/rain.js
@@ -1,10 +1,10 @@
 var demo = {
   speed: 0.95,
   color: {
-    r: '80',
-    g: '175',
-    b: '255',
-    a: '0.5'
+    r: "80",
+    g: "175",
+    b: "255",
+    a: "0.5",
   },
   started: false,
   canvas: null,
@@ -20,24 +20,24 @@ var demo = {
   rain: [],
   rain_pool: [],
   drops: [],
-  drop_pool: []
+  drop_pool: [],
 };
 
-demo.init = function() {
+demo.init = function () {
   if (!demo.started) {
     demo.started = true;
-    demo.canvas = document.getElementById('canvas');
-    demo.ctx = demo.canvas.getContext('2d');
+    demo.canvas = document.getElementById("canvas");
+    demo.ctx = demo.canvas.getContext("2d");
     var c = demo.color;
-    demo.rain_color = 'rgba(' + c.r + ',' + c.g + ',' + c.b + ',' + c.a + ')';
-    demo.rain_color_clear = 'rgba(' + c.r + ',' + c.g + ',' + c.b + ',0)';
+    demo.rain_color = "rgba(" + c.r + "," + c.g + "," + c.b + "," + c.a + ")";
+    demo.rain_color_clear = "rgba(" + c.r + "," + c.g + "," + c.b + ",0)";
     demo.resize();
     Ticker.addListener(demo.step);
   }
-}
+};
 
 // (re)size canvas (clears all particles)
-demo.resize = function() {
+demo.resize = function () {
   // localize common references
   var rain = demo.rain;
   var drops = demo.drops;
@@ -50,13 +50,13 @@ demo.resize = function() {
   }
   // resize
   demo.width = window.innerWidth;
-  demo.height = $('main').innerHeight();
+  demo.height = $("main").innerHeight();
 
   demo.canvas.width = demo.width * demo.dpr;
   demo.canvas.height = demo.height * demo.dpr;
-}
+};
 
-demo.step = function(time, lag) {
+demo.step = function (time, lag) {
   // localize common references
   var demo = window.demo;
   var speed = demo.speed;
@@ -77,7 +77,7 @@ demo.step = function(time, lag) {
     demo.drop_time -= demo.drop_delay;
     var new_rain = rain_pool.pop() || new Rain();
     new_rain.init();
-    var wind_expand = Math.abs(height / new_rain.speed * wind); // expand spawn width as wind increases
+    var wind_expand = Math.abs((height / new_rain.speed) * wind); // expand spawn width as wind increases
     var spawn_x = Math.random() * (width + wind_expand);
     if (wind > 0) spawn_x -= wind_expand;
     new_rain.x = spawn_x;
@@ -95,7 +95,11 @@ demo.step = function(time, lag) {
       r.splash();
     }
     // recycle rain
-    if (r.y > height + Rain.height * r.z || (wind < 0 && r.x < wind) || (wind > 0 && r.x > width + wind)) {
+    if (
+      r.y > height + Rain.height * r.z ||
+      (wind < 0 && r.x < wind) ||
+      (wind > 0 && r.x > width + wind)
+    ) {
       r.recycle();
       rain.splice(i, 1);
     }
@@ -110,10 +114,10 @@ demo.step = function(time, lag) {
     // apply gravity - magic number 0.3 represents a faked gravity constant
     d.speed_y += 0.3 * multiplier;
     // apply wind (but scale back the force)
-    d.speed_x += wind / 25 * multiplier;
+    d.speed_x += (wind / 25) * multiplier;
     if (d.speed_x < -drop_max_speed) {
       d.speed_x = -drop_max_speed;
-    }else if (d.speed_x > drop_max_speed) {
+    } else if (d.speed_x > drop_max_speed) {
       d.speed_x = drop_max_speed;
     }
     // recycle
@@ -124,9 +128,9 @@ demo.step = function(time, lag) {
   }
 
   demo.draw();
-}
+};
 
-demo.draw = function() {
+demo.draw = function () {
   // localize common references
   var demo = window.demo;
   var width = demo.width;
@@ -137,7 +141,7 @@ demo.draw = function() {
   var ctx = demo.ctx;
 
   // start fresh
-  ctx.clearRect(0, 0, width*dpr, height*dpr);
+  ctx.clearRect(0, 0, width * dpr, height * dpr);
 
   // draw rain (trace all paths first, then stroke once)
   ctx.beginPath();
@@ -148,7 +152,10 @@ demo.draw = function() {
     var real_y = r.y * dpr;
     ctx.moveTo(real_x, real_y);
     // magic number 1.5 compensates for lack of trig in drawing angled rain
-    ctx.lineTo(real_x - demo.wind * r.z * dpr * 1.5, real_y - rain_height * r.z);
+    ctx.lineTo(
+      real_x - demo.wind * r.z * dpr * 1.5,
+      real_y - rain_height * r.z
+    );
   }
   ctx.lineWidth = Rain.width * dpr;
   ctx.strokeStyle = demo.rain_color;
@@ -161,7 +168,7 @@ demo.draw = function() {
     var real_y = d.y * dpr - d.radius;
     ctx.drawImage(d.canvas, real_x, real_y);
   }
-}
+};
 
 // Rain definition
 function Rain() {
@@ -171,30 +178,30 @@ function Rain() {
   this.speed = 25;
   this.splashed = false;
 }
-Rain.width = 2;
+Rain.width = 3;
 Rain.height = 40;
-Rain.prototype.init = function() {
+Rain.prototype.init = function () {
   this.y = Math.random() * -100;
   this.z = Math.random() * 0.5 + 0.5;
   this.splashed = false;
-}
-Rain.prototype.recycle = function() {
+};
+Rain.prototype.recycle = function () {
   demo.rain_pool.push(this);
-}
+};
 // recycle rain particle and create a burst of droplets
-Rain.prototype.splash = function() {
+Rain.prototype.splash = function () {
   if (!this.splashed) {
     this.splashed = true;
     var drops = demo.drops;
     var drop_pool = demo.drop_pool;
 
-    for (var i=0; i<16; i++) {
+    for (var i = 0; i < 16; i++) {
       var drop = drop_pool.pop() || new Drop();
       drops.push(drop);
       drop.init(this.x);
     }
   }
-}
+};
 
 // Droplet definition
 function Drop() {
@@ -203,15 +210,22 @@ function Drop() {
   this.radius = Math.round(Math.random() * 2 + 1) * demo.dpr;
   this.speed_x = 0;
   this.speed_y = 0;
-  this.canvas = document.createElement('canvas');
-  this.ctx = this.canvas.getContext('2d');
+  this.canvas = document.createElement("canvas");
+  this.ctx = this.canvas.getContext("2d");
 
   // render once and cache
   var diameter = this.radius * 2;
   this.canvas.width = diameter;
   this.canvas.height = diameter;
 
-  var grd = this.ctx.createRadialGradient(this.radius, this.radius , 1, this.radius, this.radius, this.radius);
+  var grd = this.ctx.createRadialGradient(
+    this.radius,
+    this.radius,
+    1,
+    this.radius,
+    this.radius,
+    this.radius
+  );
   grd.addColorStop(0, demo.rain_color);
   grd.addColorStop(1, demo.rain_color_clear);
   this.ctx.fillStyle = grd;
@@ -220,26 +234,27 @@ function Drop() {
 
 Drop.max_speed = 5;
 
-Drop.prototype.init = function(x) {
+Drop.prototype.init = function (x) {
   this.x = x;
   this.y = demo.height;
-  var angle = Math.random() * Math.PI - (Math.PI * 0.5);
+  var angle = Math.random() * Math.PI - Math.PI * 0.5;
   var speed = Math.random() * Drop.max_speed;
   this.speed_x = Math.sin(angle) * speed;
   this.speed_y = -Math.cos(angle) * speed;
-}
-Drop.prototype.recycle = function() {
+};
+Drop.prototype.recycle = function () {
   demo.drop_pool.push(this);
-}
+};
 
 // Frame ticker helper module
-var Ticker = (function(){
+var Ticker = (function () {
   var PUBLIC_API = {};
 
   // public
   // will call function reference repeatedly once registered, passing elapsed time and a lag multiplier as parameters
   PUBLIC_API.addListener = function addListener(fn) {
-    if (typeof fn !== 'function') throw('Ticker.addListener() requires a function reference passed in.');
+    if (typeof fn !== "function")
+      throw "Ticker.addListener() requires a function reference passed in.";
 
     listeners.push(fn);
 
@@ -284,4 +299,4 @@ var Ticker = (function(){
   }
 
   return PUBLIC_API;
-}());
+})();
