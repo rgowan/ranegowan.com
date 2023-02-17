292bb96b0181a8869203672d0330359d3b1a5f0e
Rane Gowan <ranegowan@gmail.com>
2023-02-17 20:25:02 +0000

    add js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,59 @@
 $(loaded);
 
 function loaded() {
-  setTimeout(() => demo.init(), 1000);
-
+  demo.init();
   window.addEventListener("resize", demo.resize);
+  navBarEventListeners();
+  const navElement = document.querySelector("nav");
+
+  let prevScrollpos = window.pageYOffset;
 
   $(".recomendations-wrapper").slick({
     dots: true,
     arrows: false,
   });
 
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
+  setTimeout(() => {
+    document.querySelector("body").style.overflow = "visible";
 
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-93px";
-    }
-    prevScrollpos = currentScrollPos;
-  };
+    navElement.style.top = "0";
+    document.querySelector(".floating-email").style.left = "-72px";
+    document.querySelector(".floating-links").style.right = "0";
+
+    window.onscroll = () => {
+      var currentScrollPos = window.pageYOffset;
+
+      if (currentScrollPos !== 0) {
+        navElement.style.backgroundColor = "#fff";
+        navElement.style.borderBottom = "1px solid #f2f2f2";
+      } else {
+        navElement.style.backgroundColor = "transparent";
+        navElement.style.border = "none";
+      }
+
+      navElement.style.top = prevScrollpos > currentScrollPos ? "0" : "-93px";
+
+      prevScrollpos = currentScrollPos;
+    };
+  }, 4000);
+}
+
+function scrollIntoViewCallback(element) {
+  document
+    .querySelector(element)
+    .scrollIntoView({ behavior: "smooth", block: "center" });
+}
+
+function navBarEventListeners() {
+  document
+    .getElementById("about")
+    .addEventListener("click", () => scrollIntoViewCallback(".about"));
+
+  document
+    .getElementById("more")
+    .addEventListener("click", () => scrollIntoViewCallback(".about"));
+
+  document
+    .getElementById("recomendations")
+    .addEventListener("click", () => scrollIntoViewCallback(".recomendations"));
 }
